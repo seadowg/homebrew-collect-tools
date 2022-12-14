e306c0b81be12bf57dcd53177aa755a74023529c
Callum Stott <callum@seadowg.com>
2022-12-14 18:00:18 +0000

    Use hacky work around to install cask

--- collect-tools.rb
+++ collect-tools.rb
@@ -4,9 +4,9 @@ class CollectTools < Formula
   license "GPL-3.0-only"
   url "https://github.com/seadowg/collect-tools/releases/download/0.1.0/collect-tools-0.1.0.zip"
   sha256 "1f7f2a08aa350a04035db5cb8062b15f38cb06355d77d44f3d320ad99df23761"
-  depends_on cask: "android-platform-tools"
 
   def install
+    system "/usr/local/bin/brew", "cask", "install", "android-platform-tools"
     system "mkdir", "#{prefix}/bin/"
     system "./install.sh", "#{prefix}/bin/"
   end
