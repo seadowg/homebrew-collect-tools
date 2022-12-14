2c23126e8fee63a4d635208d3616d92e8946f3e5
Callum Stott <callum@seadowg.com>
2022-12-14 17:57:34 +0000

    Fix dependency

--- collect-tools.rb
+++ collect-tools.rb
@@ -4,7 +4,7 @@ class CollectTools < Formula
   license "GPL-3.0-only"
   url "https://github.com/seadowg/collect-tools/releases/download/0.1.0/collect-tools-0.1.0.zip"
   sha256 "1f7f2a08aa350a04035db5cb8062b15f38cb06355d77d44f3d320ad99df23761"
-  depends_on "android-platform-tools"
+  depends_on cask: "android-platform-tools"
 
   def install
     system "mkdir", "#{prefix}/bin/"
