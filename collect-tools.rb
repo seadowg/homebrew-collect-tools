e0cf76114706640e4afb238198993cf53a6a2955
Callum Stott <callum@seadowg.com>
2022-12-14 18:01:06 +0000

    Correct SHA

--- collect-tools.rb
+++ collect-tools.rb
@@ -3,7 +3,7 @@ class CollectTools < Formula
   homepage "https://github.com/seadowg/collect-tools"
   license "GPL-3.0-only"
   url "https://github.com/seadowg/collect-tools/releases/download/0.1.0/collect-tools-0.1.0.zip"
-  sha256 "1f7f2a08aa350a04035db5cb8062b15f38cb06355d77d44f3d320ad99df23761"
+  sha256 "a6c411ff5f1df8d68794d2f9af964dc241a0d29b8a76eae0d5f47fc7316d2757"
 
   def install
     system "/usr/local/bin/brew", "cask", "install", "android-platform-tools"
