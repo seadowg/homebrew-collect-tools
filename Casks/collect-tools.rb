27e5938ce023214df9d9358d884588d6482d45a1
Callum Stott <callum@seadowg.com>
2022-12-15 11:57:06 +0000

    Correct URL

--- Casks/collect-tools.rb
+++ Casks/collect-tools.rb
@@ -2,7 +2,7 @@ cask "collect-tools" do
     version "0.3.0"
     sha256 "a6c411ff5f1df8d68794d2f9af964dc241a0d29b8a76eae0d5f47fc7316d2757"
 
-    url "https://github.com/seadowg/collect-tools/releases/download/0.1.0/collect-tools-0.1.0.zip"
+    url "https://github.com/seadowg/collect-tools/releases/download/0.3.0/collect-tools-0.3.0.zip"
     name "Collect Tools"
     desc "Tools for ODK Collect"
     homepage "https://github.com/seadowg/collect-tools"
