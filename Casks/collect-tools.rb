c073701b6e252b4602958456d2472de9a7a008bd
Callum Stott <callum@seadowg.com>
2022-12-15 11:56:06 +0000

    Update for new release

--- Casks/collect-tools.rb
+++ Casks/collect-tools.rb
@@ -1,5 +1,5 @@
 cask "collect-tools" do
-    version "0.2.0"
+    version "0.3.0"
     sha256 "a6c411ff5f1df8d68794d2f9af964dc241a0d29b8a76eae0d5f47fc7316d2757"
 
     url "https://github.com/seadowg/collect-tools/releases/download/0.1.0/collect-tools-0.1.0.zip"
@@ -10,5 +10,6 @@ cask "collect-tools" do
     depends_on cask: "android-platform-tools"
 
     binary "#{staged_path}/cplayer"
+    binary "#{staged_path}/pulldir"
 end
 
